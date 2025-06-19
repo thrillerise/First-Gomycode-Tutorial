9b83cef23e9b687ce30a24bf69d6325ac141a776
Ayobami Alalade <ayobamialalade4@gmail.com>
2025-06-19 17:02:49 +0100

    THIS MY SQL PROJECT FROM GOMY CODE

--- assignment/SQL PROJECT.sql
+++ assignment/SQL PROJECT.sql
@@ -1,6 +1,6 @@
 
----ASSESSMENT Display in descending order of seniority the male employees whose net salary (salary + commission) is greater than or equal to 8000.
---The resulting table should include the following columns: Employee Number, First Name and Last Name, Age, and Seniority.
+/*ASSESSMENT Display in descending order of seniority the male employees whose net salary (salary + commission) is greater than or equal to 8000.
+--The resulting table should include the following columns: Employee Number, First Name and Last Name, Age, and Seniority.*/
 SELECT first_name, last_name 
 FROM EMPLOYEES
 WHERe (salary + commission) >= 8000;
@@ -9,7 +9,7 @@ select *, (salary + commission) as net_com
 from employees
 
 
---question 2
+/*--question--*/
 /* Display products that meet the following criteria: (C1) quantity is packaged in bottle(s), (C2) the third character in the product name is 't' or 'T', 
 (C3) supplied by suppliers 1, 2, or 3, (C4) unit price ranges between 70 and 200, and (C5) units ordered are specified (not null). 
 The resulting table should include the following columns: product number, product name, supplier number, units ordered, and unit price. */
@@ -40,7 +40,7 @@ and right(postal_code, 3) =(
 						from suppliers
 						where supplier_int = 1);
 
-/* Queston 3
+/* Queston 4
 For each order number between 10998 and 11003, do the following:  
 -Display the new discount rate, which should be 0% if the total order amount before discount (unit price * quantity) 
 is between 0 and 2000, 5% if between 2001 and 10000, 10% if between 10001 and 40000, 15% if between 40001 and 80000, and 20% otherwise.
@@ -72,8 +72,8 @@ from order_details;
 
 
 
---question 4
---Display suppliers of beverage products. The resulting table should display the columns: supplier number, company, address, and phone number.
+/*question 5
+--Display suppliers of beverage products. The resulting table should display the columns: supplier number, company, address, and phone number.*/
 select category_code,company, address, phone number,s.supplier_int
 from products as p
 full outer join  suppliers as s
@@ -81,8 +81,8 @@ on p.supplier_int = s.supplier_int
 where category_code = 1
 order by p.supplier_int;
 
---question 5
---Display customers from Berlin who have ordered at most 1 (0 or 1) dessert product. The resulting table should display the column: customer code.
+/*question 6
+--Display customers from Berlin who have ordered at most 1 (0 or 1) dessert product. The resulting table should display the column: customer code.*/
 UPDATE customers
 set country ='Berlin'
 where country ='Brésil';
@@ -104,7 +104,7 @@ on o.order_int= od.order_int
 where country = 'Berlin';
 
 
---question 6
+/*question 7*/
 -- Display customers who reside in France and the total amount of orders they placed every Monday in April 1998 (considering customers who haven't placed any orders yet). 
 -- The resulting table should display the columns: customer number, company name, phone number, total amount, and country.
 select c.customer_code, c.company, c.country,count(o.customer_code) cust_c
@@ -116,14 +116,45 @@ where country ='France'
 group by c.customer_code, c.country
 
 
----question 7
+/*---question 8
 ---Display customers who have ordered all products. The resulting table should display the columns: customer code, company name, and telephone number.
+*/
+SELECT c.customer_code, c.company, c.phone
+FROM customers c
+INNER JOIN orders o
+ON c.customer_code = o.customer_code;
+
+/* QUESTION 9
+Display for each customer from France the number of orders they have placed. The resulting table should display the columns: customer code and number of orders.
+*/
+
+SELECT c.customer_code AS customer_code, COUNT(o.customer_code) AS number_of_orders
+FROM orders o 
+LEFT JOIN customers c
+ON o.customer_code = c.customer_code
+WHERE c.country = 'France'
+GROUP BY c.customer_code;
+
+
+/*Display the number of orders placed in 1996, the number of orders placed in 1997,
+ and the difference between these two numbers. The resulting table should display the columns: orders in 1996,
+  orders in 1997, and Difference.*/
+
+SELECT 
+    COUNT(CASE WHEN EXTRACT(YEAR FROM order_date) = 1996 THEN 1 END) AS "orders in 1996",
+    COUNT(CASE WHEN EXTRACT(YEAR FROM order_date) = 1997 THEN 2 END) AS "orders in 1997",
+    COUNT(CASE WHEN EXTRACT(YEAR FROM order_date) = 1997 THEN 2 END) -
+    COUNT(CASE WHEN EXTRACT(YEAR FROM order_date) = 1996 THEN 1 END) AS "Difference"
+FROM orders;
 
 select *
 from customers
 
 select *
 from orders
+
+select *
+from order_details
 
 select *
 from products
